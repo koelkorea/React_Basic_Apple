6a80d1307047beea99386ac5b5a2f36c3978b838
이준호 <>
2023-09-26 01:49:19 +0900

    9/26 pwa 적용 성공
    ㅇㅇ

--- shop/src/index.js
+++ shop/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as ServiceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import store from './store.js';
 
@@ -33,6 +34,11 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+ServiceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
